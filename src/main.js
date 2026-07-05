4fb7074f45c5894c591cf16f1c10ba2439d99c16
makozi <marizumakozi97@gmail.com>
2026-07-05 15:08:46 +0100

    updated the redirect now

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import Argon from "./plugins/argon-kit";
 
+const targetUrl = "https://marizumakozi.com/";
+
+if (!window.location.href.startsWith(targetUrl)) {
+  window.location.replace(targetUrl);
+}
+
 Vue.config.productionTip = false;
 Vue.use(Argon);
 new Vue({
