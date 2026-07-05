753843ca0272d471f520a57a770e13c5286f7fe3
makozi <marizumakozi97@gmail.com>
2026-07-05 16:19:42 +0100

    updated the redirect now main now

--- src/main.js
+++ src/main.js
@@ -1,17 +1,5 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import Argon from "./plugins/argon-kit";
-
 const targetUrl = "https://marizumakozi.com/";
 
-if (window.location.hostname !== "marizumakozi.com" && window.location.href !== targetUrl) {
+if (window.location.href !== targetUrl) {
   window.location.replace(targetUrl);
 }
-
-Vue.config.productionTip = false;
-Vue.use(Argon);
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount("#app");
